89734358030a6c24a5581a38266b93d5d942c6bb
Alexander Lindt <alex.lindt@t-online.de>
2026-08-08 22:21:23 +0200

    feat: add  enviroments,.ts  ,  mobil image and style

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://localhost:5678',
+  recipeWebhookUrl: 'http://localhost:5678/webhook/',
+  firebaseDatabaseUrl: 'https://code-a-cuisine-ccf1f-default-rtdb.firebaseio.com',
+};
